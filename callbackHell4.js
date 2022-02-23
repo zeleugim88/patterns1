08897f89c059c43db5faae151bb3e9ca1ca4bfad
DANI MI <zeleugim@gmail.com>
2022-02-23 21:41:27 +0100

    20220203

--- callbackHell4.js
+++ callbackHell4.js
@@ -27,16 +27,16 @@ const reverseText = str =>  str  .split("")  .reverse()  .join("");
   });
 }); */
 
-async function leerDirectorio(inbox) {
+function leerDirectorio(inbox) {
   return new Promise((resolve, reject) => {
       readdir(inbox, (error, files) => {
-        if (error) reject (console.log("Error: Folder inaccessible")); 
+        if (error) reject (new Error("Error: Folder inaccessible")); 
         resolve(files) 
   });
- })
+ });
 }
 
- async function leerArchivo(file) {
+ function leerArchivo(file) {
   return new Promise((resolve, reject) => {
     readFile(join(inbox, file), "utf8", (error, data) => {
     if (error) reject(console.log("Error: File error"));
@@ -45,21 +45,22 @@ async function leerDirectorio(inbox) {
  })
 }
 
- async function escribirArchivo(data) {
+ function escribirArchivo(file, data) {
    return new Promise((resolve, reject) => {
     writeFile(join(outbox, file), reverseText(data), error => {
-      if (error) reject(console.log("Error: File could not be saved!"));
-      resolve(`${file} was successfully saved in the outbox!`);
+      if (error) reject(new Error("Error: File could not be saved!"));
+      resolve(console.log(`${file} was successfully saved in the outbox!`));
    })
  })
 }
 
-async function finalResult(){
-  let files = await leerDirectorio(inbox);
+async function resultadoFinal(){
+  const files = await leerDirectorio(inbox);
     //for (i=0; i< files.length; i++) {
       files.forEach(async file => {
-      let data = await leerArchivo(files[i]);
-      let finalResult = await escribirArchivo(data);
-      console.log(finalResult)
+      let data = await leerArchivo(file);
+      await escribirArchivo(file, data);
     })
   }
+
+  resultadoFinal();
